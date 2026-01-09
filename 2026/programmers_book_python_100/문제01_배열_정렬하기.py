8776c7e663c74f1b605190f2fc609794d3d7b5e2
yourseong <korea020808@naver.com>
2026-01-09 16:12:30 +0900

    [260109] 문제01 - 배열 정렬하기

---
+++ 2026/programmers_book_python_100/문제01_배열_정렬하기.py
@@ -0,0 +1,16 @@
+# 정수 배열을 오름차순 정렬해서 반환하는 solution 함수 완성하기
+
+# 입력을 받고싶으면 작성
+arr = list(map(int, input().split(",")))
+
+def solution(arr):
+    arr.sort()
+    return arr
+
+print("입력을 받을 때: ", solution(arr))
+print("그냥 실행할 때")
+
+# 테스트케이스
+print(solution([1,-5,2,4,3])) # 반환값 : [-5, 1, 2, 3, 4]
+print(solution([2,1,1,3,2,5,4])) # 반환값 : [1, 1, 2, 2, 3, 4, 5]
+print(solution([1,6,7])) # 반환값 : [1, 6, 7]
