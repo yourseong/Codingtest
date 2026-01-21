a8951bd28845e37752b899c68435c6fd2bb685cb
chaeyuseong@naver.com <spitz910325@naver.com>
2026-01-21 10:36:18 +0900

    test commit

---
+++ 2026/programmers_book_python_100/문제05_행렬의 곱셈.py
@@ -0,0 +1,15 @@
+# 250115(목)
+# 2차원 행렬 arr1과 arr2를 입력받아 arr1 * arr2 결과를 반환하는 solution() 함수 완성하기
+
+# def solution(arr1, arr2):
+
+#     return
+
+
+
+
+# print(solution([[1,4],[3,2],[4,1]], [[3,3],[3,3]]))
+
+arr1 = list([[1,4],[3,2],[4,1]])
+arr2 = list([[3,3],[3,3]])
+print(arr1[0][0]*arr2[0][0])
